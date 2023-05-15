f08b4db7fd757d3ec36a0351db43192ecf4d4486
Sematemur <99514111+Sematemur@users.noreply.github.com>
2023-05-15 22:37:27 +0300

    Add files via upload

---
+++ çinzodyağı.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class çinzodyağı{
+public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.println("doğum yilinizi girin");
+    int yil=input.nextInt();
+    int islem=yil%12;
+    if(islem==0)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :maymun");
+    }
+    if(islem==1)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :horoz");
+    }
+    if(islem==2)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :köpek");
+    }
+    if(islem==3)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :domuz");
+    }
+    if(islem==4)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :fare");
+    }
+    if(islem==5)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :öküz");
+    }
+    if(islem==6)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :kaplan");
+    }
+    if(islem==7)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :tavsan");
+    }
+    if(islem==8)
+    {
+      System.out.println("Çin Zodyağı Burcunuz : ejderha");
+    }
+    if(islem==9)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :yılan");
+    }
+    if(islem==10)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :at");
+    }
+    if(islem==11)
+    {
+      System.out.println("Çin Zodyağı Burcunuz :koyun");
+    }
+
+
+}
+}
