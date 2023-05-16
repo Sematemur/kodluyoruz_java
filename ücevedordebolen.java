c15778b76f127e889707f3bc5e6898c036e138a8
Sematemur <99514111+Sematemur@users.noreply.github.com>
2023-05-16 19:30:27 +0300

    ücevedordebolen.java

--- ücevedordebolen.java
+++ ücevedordebolen.java
@@ -4,12 +4,16 @@ public class ücevedordebolen {
         Scanner input=new Scanner(System.in);
         System.out.println("sayiyi  girin");
         int sayi=input.nextInt();
+         int islem=0;
+        int sayac=0;
         for(int i=0;i<=sayi;i++)
         {
          if(i%3 ==0 && i%4==0)
          {
             System.out.println(i+" : 3 e ve 4 e bolunebilir");
          }
+            islem=islem/sayac;
+         System.out.println("islem");
          
         }
     
